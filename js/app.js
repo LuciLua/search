95a8beced300deda5fd5213b5fc9d892ebd533af
LuciLua <luci.lua81@gmail.com>
2021-09-12 18:10:39 -0300

    :cactus: add new words in array

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function buscarPalavrasSemelhantes(palavra, array){
     var array = document.getElementById('array').textContent.split(',')
     var palavra = document.getElementById('search').value
     var result = document.querySelector('.result')
-    result.innerHTML = '' //reseta
+    result.innerHTML = '' //reset
     for(let i = 0; i < array.length; i++){
         var item = (array.filter(e => e.includes(palavra)))[i]
         if(item == undefined || palavra == ''){
@@ -14,5 +14,8 @@ function buscarPalavrasSemelhantes(palavra, array){
 }
 
 function add(){
-    
+    var addWord = document.getElementById('add').value
+    if(addWord.length != 0){
+        var array = document.getElementById('array').textContent += ', '+ addWord
+    }
 }
